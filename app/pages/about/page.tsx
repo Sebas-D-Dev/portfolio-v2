133695077d67361f235c27f1f6f8f678f74e0e62
Sebas-D-Dev <sebas.t.nait@gmail.com>
2025-05-18 18:24:37 -0400

    New scroll bar, animation direction, and icons

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -5,9 +5,16 @@ import Image from "next/image";
 import "../../styles/about.css";
 
 // List of tech stacks with corresponding icons
-const techStacks = [
+const techLanguages = [
   { name: "Python", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/python.svg" : "/assets/python.svg" },
   { name: "JavaScript", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/javascript.svg" : "/assets/javascript.svg" },
+  { name: "TypeScript", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/typescript.svg" : "/assets/typescript.svg" },
+  { name: "HTML", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/html.svg" : "/assets/html.svg" },
+  { name: "CSS", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/css.svg" : "/assets/css.svg" },
+  { name: "SQL", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/sql.svg" : "/assets/sql.svg" },
+  { name: "Java", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/java.svg" : "/assets/java.svg" },
+];
+const techStacks = [
   { name: "MongoDB", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/mongodb.svg" : "/assets/mongodb.svg" },
   { name: "React", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/react.svg" : "/assets/react.svg" },
   { name: "Next.js", icon: process.env.NODE_ENV === "production" ? "/portfolio-v2/assets/nextjs.svg" : "/assets/nextjs.svg" },
@@ -40,7 +47,25 @@ export default function About() {
 
         {/* Tech Stack Scroll Bar Container */}
         <div className="scroll-container">
-          <div className="scroll-wrapper animate-scroll">
+          <div className="scroll-wrapper animate-scroll-left">
+            {techLanguages.map((tech) => (
+              <div key={tech.name} className="tech-item gradient-bg">
+                <Image src={tech.icon} alt={tech.name} width={50} height={50} />
+                <p>{tech.name}</p>
+              </div>
+            ))}
+            {/* Duplicate items for seamless loop */}
+            {techLanguages.map((tech) => (
+              <div key={`${tech.name}-duplicate`} className="tech-item gradient-bg">
+                <Image src={tech.icon} alt={tech.name} width={50} height={50} />
+                <p>{tech.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="scroll-container">
+          <div className="scroll-wrapper animate-scroll-right">
             {techStacks.map((tech) => (
               <div key={tech.name} className="tech-item gradient-bg">
                 <Image src={tech.icon} alt={tech.name} width={50} height={50} />
