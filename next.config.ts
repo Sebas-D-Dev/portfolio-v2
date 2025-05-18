cb83e41b36409ad6af1f718d54f226fdac721a0c
Sebas-D-Dev <sebas.t.nait@gmail.com>
2025-05-18 02:05:28 -0400

    fix image loading

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+// Check if in npm dev or production environment
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  // Update image config path
+  basePath: isProd ? '/portfolio-v2' : '',
   // config altered to static export for GitHub Pages deployment
   output: "export",
   // Output directory for static files
